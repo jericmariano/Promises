aec6131467baac0145710fde980e76e6c2ecb34d
Jeric <marianovjeric@gmail.com>
2022-11-15 20:26:51 -0800

    first method then in promises

---
+++ app.js
@@ -0,0 +1,18 @@
+// fetch("https://jsonplaceholder.typicode.com/users/1")
+
+const emailRef = document.querySelector(".email")
+
+
+// 1. Then
+
+fetch("https://jsonplaceholder.typicode.com/users/1")
+    .then(response => {
+        return response.json()
+    })
+    .then(data => {
+        console.log(data)
+        emailRef.innerHTML = data.email
+    })
+
+
+// 2. Async/Await
